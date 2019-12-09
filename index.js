b4ec803aa931f8ac5af93e043e8cb8970a88f768
Peter Rufo <peter@peterrufo.com>
2019-12-09 16:11:53 -0500

    fix

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ function displayResults(responseJson) {
 function displayCocktail(){
   $('#results-list').on('click', '.drink-image', function () {
     // console.log($(this).parent().siblings()[1]);
+    event.preventDefault();
     $(this).parent().siblings().toggle(800); 
   });
   // $('#results-list').on('submit', '.drink-image', function () {
